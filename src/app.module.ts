0925f46b91058dabe3fafb3c355a6ad359423661
NotACat1 <notacat.mail@gmail.com>
2024-08-28 17:04:15 +0300

    feat: создание API

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,25 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import ormConfig from '@config/typeorm.config';
+import { AuthModule } from '@modules/auth/auth.module';
+import { HashingModule } from '@modules/hashing/hashing.module';
+import { OffersModule } from '@modules/offers/offers.module';
+import { UsersModule } from '@modules/users/users.module';
+import { WishesModule } from '@modules/wishes/wishes.module';
+import { WishlistsModule } from '@modules/wishlists/wishlists.module';
+import { AppController } from '@/app.controller';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot(ormConfig),
+    AuthModule,
+    OffersModule,
+    UsersModule,
+    WishesModule,
+    WishlistsModule,
+    HashingModule,
+  ],
   controllers: [AppController],
   providers: [],
 })
